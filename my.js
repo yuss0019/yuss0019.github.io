edf526d051dfe70a0b61df006fb5873ba8d20ed1
yuss0019 <42668395+yuss0019@users.noreply.github.com>
2018-08-24 16:13:40 +0800

    Add files via upload

---
+++ my.js
@@ -0,0 +1,30 @@
+
+var username ="techcove"
+alert("hello" + username)
+
+var email = prompt("you won one dollar")
+
+alert("your email is" + email)
+
+
+var firstname = prompt("what your first name?")
+var lastname = prompt("what is your last name?")
+var age = prompt("what is your age?")
+
+
+alert("hi " + firstname +" "+lastname +age)
+
+
+
+var number = prompt("what is the number")
+if(number<18){
+	alert("cannot enter")
+}else if(number<21){
+	alert("not allow to drink")
+}else{
+	alert("let's party")
+}
+
+
+
+
